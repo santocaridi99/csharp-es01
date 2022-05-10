640437f9357cfba15490c8aa1ae0b3a9311df329
santocaridi99 <santocaridi99@gmail.com>
2022-05-10 17:25:17 +0200

    Aggiungere i file di progetto.

---
+++ csharp-es01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)  //entry point
+        {
+            //namespace, classe, metodo
+            System.Console.WriteLine("prodotto tra i due numeri: {0}",
+                Mol());
+        }
+        static int Mol()
+        {
+            int v1, v2;
+            System.Console.WriteLine("inserisci primo numero");
+            //int v1 = int.Parse(Console.ReadLine());
+            if (!int.TryParse(Console.ReadLine(), out v1))
+            {
+                System.Console.WriteLine("Non hai inserito un intero");
+                System.Environment.Exit(1);
+
+            }
+            System.Console.WriteLine("inserisci secondo numero");
+            //int v2 = int.Parse(Console.ReadLine());
+            if (!int.TryParse(Console.ReadLine(), out v2))
+            {
+                System.Console.WriteLine("Non hai inserito un intero");
+                System.Environment.Exit(1);
+
+            }
+            return v1 * v2;
+        }
+    }
+}
